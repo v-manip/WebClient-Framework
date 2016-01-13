61702d4d6eef79556bd6e3e6e96e47daef156a74
Martin Paces <martin.paces@eox.at>
2016-01-13 14:31:01 +0100

    Displaying client version in the about view.

--- app/scripts/globals.js
+++ app/scripts/globals.js
@@ -2,6 +2,7 @@
 // globals
 define(['backbone', 'objectStore'], function(Backbone, ObjectStore) {
 	return {
+		version: "0.2.0dev0",
 		objects: new ObjectStore(),
 		selections: new ObjectStore(),
 		baseLayers: new Backbone.Collection(),
