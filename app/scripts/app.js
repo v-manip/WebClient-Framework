0b2357a9983b42ea78a6a0dcaf2b4d0aa04da5db
Daniel Santillan <daniel.santillan@eox.at>
2016-03-18 15:52:36 +0100

    Added check for CSRF code to be added in ajax requests; Removed selectionmanager from configuration;

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -421,6 +421,39 @@
 					this.storyView.show(this.storyBanner);
 				}*/
 
+
+				// Try to get CSRF token, if available set it for necesary ajax requests
+				function getCookie(name) {
+				    var cookieValue = null;
+				    if (document.cookie && document.cookie != '') {
+				        var cookies = document.cookie.split(';');
+				        for (var i = 0; i < cookies.length; i++) {
+				            var cookie = jQuery.trim(cookies[i]);
+				            // Does this cookie string begin with the name we want?
+				            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+				                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+				                break;
+				            }
+				        }
+				    }
+				    return cookieValue;
+				}
+				var csrftoken = getCookie('csrftoken');
+
+				if(csrftoken){
+					function csrfSafeMethod(method) {
+					    // these HTTP methods do not require CSRF protection
+					    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+					}
+					$.ajaxSetup({
+					    beforeSend: function(xhr, settings) {
+					        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+					            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+					        }
+					    }
+					});
+				}
+
 			    // Add a trigger for ajax calls in order to display loading state
                 // in mouse cursor to give feedback to the user the client is busy
                 $(document).ajaxStart(function() {
